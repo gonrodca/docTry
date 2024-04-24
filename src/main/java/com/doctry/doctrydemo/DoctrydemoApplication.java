2282bbbb8e2647e531f36e1ad38db108997682f5
gonrodca <gonzalo-r@hotmail.es>
2024-04-24 20:28:47 -0300

    aa

---
+++ src/main/java/com/doctry/doctrydemo/DoctrydemoApplication.java
@@ -0,0 +1,13 @@
+package com.doctry.doctrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctrydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctrydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctry/doctrydemo/DoctrydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctry.doctrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctrydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
