4b5e8cb95a8b3fc385ffc800ed6ece56620fb47f
gonrodca <gonzalo-r@hotmail.es>
2024-04-24 21:38:54 -0300

    a

--- src/Packages/Controlador/ControladorUsuario.java
+++ src/Packages/Controlador/ControladorUsuario.java
@@ -2,6 +2,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,11 @@ public class ControladorUsuario {
         }
 
         return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario user){
+        return ResponseEntity.ok(usuarioService.save(user));
     }
 
 }
